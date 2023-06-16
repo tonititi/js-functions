e1d95fa2707ac0b2ef645a33967b5ec0ee39394e
tonititi <dong.vu.cong@gmail.com>
2023-06-16 17:00:48 +0700

    add log test

--- blake.js
+++ blake.js
@@ -1,6 +1,36 @@
+const {
+  utils,
+  helpers,
+  CasperContractClient,
+} = require("casper-js-client-helper");
+
+const {
+  CLValueBuilder,
+  CLStringBytesParser,
+  CLPublicKey,
+  CLKey,
+  CLString,
+  CasperClient,
+  CLByteArray,
+  RuntimeArgs,
+  CLAccountHash,
+  DeployUtil,
+  Keys,
+  CLTypeBuilder,
+} = require("casper-js-sdk");
 let blake = require("blakejs")
-let a = "hash-be170557d9100704b63dc5de6039373dfdc8649cc467bf2f74ea14812d233def";
-let b = "11";
+const BigNumber = require("bignumber.js")
+let a1 = "e3f6bf7f868a25f7b84597009158a1c2cea8df9bbcb949e0405c55c1a2b55c0b";
+let a = "f0f91595bc63e1ce2f015dbacdd816619f63053cdf5fb41f19d69ffecbba755f"
+let b = "15";
+
+function hexToBytes(hex) {
+  let bytes = [];
+  for (let c = 0; c < hex.length; c += 2)
+    bytes.push(parseInt(hex.substr(c, 2), 16));
+  return bytes;
+}
+
 function strToBytes(str) {
   const bytes = [];
   for (ii = 0; ii < str.length; ii++) {
@@ -9,11 +39,32 @@ function strToBytes(str) {
   }
   return bytes;
 }
+function u256ToBytes(strx) {
+  let a = new BigNumber(strx).toString(16)
+  a = a.length % 2 == 0 ? a : "0" + a
+  let b = Buffer.from(a, 'hex')
+  const ret = [b.length, ...b]
+  return ret;
+}
+console.log("aa test push")
 
-let aBytes = strToBytes(a)
+
+
+// console.log(u256ToBytes(1000))
+// let aBytes = Buffer.from("55884917f4107a59e8c06557baee7fdada631af6d1c105984d196a84562854eb", "hex")
+// let a1Bytes = strToBytes(a1)
+// let aBytes = strToBytes(a)
+
+let a1Bytes = hexToBytes(a1)
+console.log(a1Bytes)
+let aBytes = hexToBytes(a)
+// let x = new CLStringBytesParser().toBytes(new CLString("14")).val//.toBytes()
+// console.log("x", x)
+// let bBytes =  [2, 0, 0, 0, 49, 52 ]// strToBytes("23")
+// let finalS1 = [0, ...aBytes, ...x]
 let bBytes = strToBytes(b)
-let finalS1 = aBytes.concat(bBytes)
-let finalS2 = bBytes.concat(aBytes)
+let finalS1 = a1Bytes.concat(aBytes)
+let finalS2 = aBytes.concat(a1Bytes)
 console.log('finalS1', Buffer.from(finalS1).toString('hex'), finalS1.length)
 console.log('finalS1', Buffer.from(finalS2).toString('hex'))
 console.log(a.length)
